91a8095bdd83bb0e3c8ebc4b80c56c1f7e286369
AlexAgruso <agrusoalexander@gmail.com>
2023-06-01 22:45:38 -0500

    base navbar

---
+++ src/lib/components/Navbar.svelte
@@ -0,0 +1,28 @@
+<nav class="row">
+    <h1>Matt Schewe Golf</h1>
+    <div class="row links">
+        <a>navbar</a>
+        <a>navbar</a>
+        <a>navbar</a>
+        <a>navbar</a>
+    </div>
+    <h2>Contact Info</h2>
+</nav>
+
+<style lang="scss">
+    nav {
+        justify-content: space-between;
+        align-items: center;
+
+        padding: 1rem;
+    }
+
+    .links {
+        gap: 1rem;
+    }
+
+    a {
+        color: gray;
+        letter-spacing: 0.125rem;
+    }
+</style>
--- svelte.config.js
+++ svelte.config.js
@@ -1,18 +1,35 @@
 import adapter from '@sveltejs/adapter-auto';
-import { vitePreprocess } from '@sveltejs/kit/vite';
+import preprocess from "svelte-preprocess";
+
+import path from "path";
+import { fileURLToPath } from "url";
+
+const scssPath = `${path.dirname(fileURLToPath(import.meta.url))}/src/scss`;
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	// Consult https://kit.svelte.dev/docs/integrations#preprocessors
-	// for more information about preprocessors
-	preprocess: vitePreprocess(),
-
 	kit: {
-		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
-		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
-		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+		alias: {
+            "@components/*": "./src/components/*",
+            "@scss/*": "./src/scss/*",
+            "@lib/*": "./src/lib/*",
+        },
+	},
+	onwarn: (warning, handler) => {
+        const { code } = warning;
+
+        if (code === "css-unused-selector") {
+            return;
+        }
+
+        handler(warning);
+    },
+    preprocess: preprocess({
+        scss: {
+            prependData: `@import "${scssPath}/_global";`,
+        },
+    }),
 };
 
 export default config;
