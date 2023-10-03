9ae405921e6f53735915f805c1a8ede191e37b46
alexagruso <agrusoalexander@gmail.com>
2023-10-03 06:44:00 -0500

    squash this

--- src/lib/components/layout/Navbar.svelte
+++ src/lib/components/layout/Navbar.svelte
@@ -1,4 +1,5 @@
 <script lang="ts">
+    let showDropdown = false;
 </script>
 
 <nav class="row">
@@ -12,12 +13,21 @@
                 </p>
             </div>
         </div>
-        <div class="row links">
+        <div class:active={showDropdown} class="row links">
             <a href="/">Home</a>
             <a href="/about">About</a>
             <a href="/lessons">Lessons</a>
             <a href="/blog">Blog</a>
         </div>
+        <button
+            class="col hamburger"
+            on:click={() => {
+                showDropdown = !showDropdown;
+            }}>
+            <div class:active={showDropdown} class="slice" id="a" />
+            <div class:active={showDropdown} class="slice" id="b" />
+            <div class:active={showDropdown} class="slice" id="c" />
+        </button>
     </div>
 </nav>
 
@@ -26,9 +36,10 @@
         position: sticky;
         top: 0rem;
 
+        justify-content: center;
         align-items: center;
 
-        width: 100%;
+        padding: 0.5rem;
 
         background-color: $accent-2;
 
@@ -38,9 +49,8 @@
     .content {
         justify-content: space-between;
         align-items: center;
-        flex-grow: 1;
 
-        padding: 0.5rem 8rem;
+        width: 95rem;
     }
 
     .logo {
@@ -86,15 +96,39 @@
 
     .links {
         align-items: center;
-        gap: 1rem;
+        gap: 0.5rem;
+
+        @include tablet-portrait-and-below {
+            position: absolute;
+            top: -100vh;
+            right: 0;
+            flex-direction: column;
+            align-items: center;
+
+            display: flex;
+
+            opacity: 0;
+            transition: opacity 100ms, top 200ms;
+
+            width: 100%;
+
+            background-color: rgba($accent-2, 50%);
+
+            &.active {
+                top: 0rem;
+
+                opacity: 1;
+            }
+        }
     }
 
     a {
-        transition: background-color 150ms;
+        transition: background-color 100ms, color 100ms;
 
-        border-radius: 0.75rem;
-        padding: 0.5rem 2rem;
+        border-radius: 1rem;
+        padding: 0.5rem 1.5rem;
 
+        text-align: center;
         text-transform: lowercase;
         color: $primary-6;
 
@@ -103,5 +137,44 @@
         &:hover {
             background-color: rgba($accent-4, 50%);
         }
+    }
+
+    .active a {
+        width: 100%;
+    }
+
+    .hamburger {
+        display: none;
+
+        margin-right: 1rem;
+
+        @include tablet-portrait-and-below {
+            display: flex;
+            gap: 0.5rem;
+
+            background-color: $accent-2;
+        }
+    }
+
+    .slice {
+        transition: transform 200ms, opacity 100ms;
+
+        border-radius: 1rem;
+        width: 2.75rem;
+        height: 0.375rem;
+
+        background-color: $primary-6;
+    }
+
+    #a.active {
+        transform: translateY(0.875rem) rotateZ(43deg);
+    }
+
+    #b.active {
+        opacity: 0;
+    }
+
+    #c.active {
+        transform: translateY(-0.875rem) rotateZ(-43deg);
     }
 </style>
