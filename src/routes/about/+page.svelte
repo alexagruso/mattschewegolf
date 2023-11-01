bea2947ea0922e3abad9486914b733c01d901e9f
alexagruso <agrusoalexander@gmail.com>
2023-11-01 18:48:29 -0500

    update bio 2023 award

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -6,16 +6,20 @@
         <h1 class="title">Biography</h1>
         <h2 class="subtitle">About matt</h2>
         <div class="col text">
+            <p class="notice">
+                Matt Schewe has been announced as the 2023 STPGA Eastern Chapter's Teacher of the Year, and has won the
+                2023 Harvey Penick Teacher of the Year Award!
+            </p>
             <p>
-                Matt Schewe has received the prestigious 2020, 2021, and 2022 Southern Texas PGA teacher of the year
-                nomination, landing in the top 5 each year. Matt has become one of the most sought-after PGA instructors
-                in the Houston/Pearland area. Throughout his career, Matt has worked with famous golf instructors Peter
-                Kostis and Dr. Gary Wiren. Not long after graduating from the Professional Golf Management Program at
-                Ferris State University, Matt was hired as the Head Golf Professional and Director of Instruction at
-                Texas A & M University. Matt&apos;s success followed him to Pearland Golf Club where he continued The
-                Matt Schewe School of Golf. The Houston Press named him 2009 Teacher of the Year. He has students as
-                young as 4 years old. He has many high school players that have received golf scholarships to major
-                colleges.
+                Matt has received nominations from the prestigious Southern Texas PGA teacher of the year awards for the
+                past four years, landing in the top 5 each year. He has also become one of the most sought-after PGA
+                instructors in the Houston/Pearland area. Throughout his career, Matt has worked with famous golf
+                instructors Peter Kostis and Dr. Gary Wiren. Not long after graduating from the Professional Golf
+                Management Program at Ferris State University, Matt was hired as the Head Golf Professional and Director
+                of Instruction at Texas A & M University. Matt&apos;s success followed him to Pearland Golf Club where
+                he continued The Matt Schewe School of Golf. The Houston Press named him 2009 Teacher of the Year. He
+                has students as young as 4 years old. He has many high school players that have received golf
+                scholarships to major colleges.
             </p>
             <p>
                 Matt believes in an “effortless golf swing” that is smooth and easy. He can teach anyone from small
@@ -105,12 +109,14 @@
     .text {
         gap: 1.5rem;
 
-        // padding: 1rem;
-
         color: $primary-2;
 
         p {
             text-align: justify;
         }
+    }
+
+    .notice {
+        font-size: 150%;
     }
 </style>
